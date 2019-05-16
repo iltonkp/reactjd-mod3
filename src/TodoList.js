5fdae4f792dbf8cee534cc2c9a46464fee9e6dab
iltonkp <iltonk.si@gmail.com>
2019-05-16 20:11:47 -0300

    GoReact - mod3 - Configurando reducers para o todolist

---
+++ src/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = () => (
+  <ul>
+    <li>Fazer Café</li>
+    <li>Estudar React</li>
+    <li>Entrar na Comunidade</li>
+  </ul>
+);
+
+export default TodoList;
