ec021b6c7cbb0e59840fed573db47d5a74e0e17e
iltonkp <iltonk.si@gmail.com>
2019-05-17 20:31:37 -0300

    GoReact - mod3 - Removendo TO-DOs

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,11 +6,17 @@ import { connect } from 'react-redux';
 
 import * as TodoActions from './store/actions/todos';
 
-const TodoList = ({ todos, addTodo }) => (
+const TodoList = ({ todos, addTodo, removeTodo }) => (
   <Fragment>
     <ul>
       {todos.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          {todo.text}
+          <button type="button" onClick={() => removeTodo(todo.id)}>
+            {' '}
+            Remover
+          </button>
+        </li>
       ))}
     </ul>
 
@@ -23,6 +29,7 @@ const TodoList = ({ todos, addTodo }) => (
 
 TodoList.propTypes = {
   addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
